abb1d79d4f969e9818d5793ff955db0196e1300c
Sultonbek <idrisovsultonbek@gmail.com>
2023-04-14 14:20:36 +0500

    Odd1.dart

--- isOdd.dart
+++ isOdd.dart
@@ -1,4 +1,9 @@
 // Create a variable "number" and assign it a integer value.
 
 // Print 1 if the number is odd, otherwise print 0.
-void main() {}
+void main() {
+  int a = 21;
+  int b = 21;
+  print(a ~/ b);
+  print(a %b );
+}
